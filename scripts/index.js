55d32a325c81362681a632f852f25a023643ce4f
= <=>
2025-02-18 13:34:06 +0500

    ТЗ 5.1

--- scripts/index.js
+++ scripts/index.js
@@ -17,52 +17,61 @@ document.querySelectorAll(".popup__close").forEach(button => {
 // Поля формы добавления карточки
 const cardName = cardCreatePopup.querySelector(".popup__input_type_card-name");
 const cardUrl = cardCreatePopup.querySelector(".popup__input_type_url");
-const cardCreateSubmitButton = cardCreatePopup.querySelector(".popup__button");
+const cardCreateForm = document.querySelector('.popup__form[name="new-place"]');
+
+// Функция удаления карточки
+const removeCard = card => card.remove();
+
+// Функция добавления лайка
+const toggleLike = event => {
+    event.currentTarget.classList.toggle("card__like-button_is-active");
+};
+
+// Функция показа изображения
+const showContentImage = (name, link) => {
+    contentImage.src = link;
+    contentImage.alt = name;
+    contentCaption.textContent = name;
+    openPopup(contentImagePopup);
+};
+
+const getTemplate = () => {
+    return document
+        .querySelector("#card-template")
+        .content.querySelector(".places__item")
+        .cloneNode(true);
+};
 
 // Функция создания карточки
-const createCard = ({name, link}) => {
-    const cardTemplate = document.querySelector("#card-template").content;
-    const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+const createCard = ({name, link}, {removeCard, toggleLike, showContentImage}) => {
+    const card = getTemplate();
 
     card.querySelector('.card__title').textContent = name;
     const cardImage = card.querySelector('.card__image');
     cardImage.src = link;
     cardImage.alt = name;
 
-    cardImage.addEventListener("click", () => showContentImage(card));
-    card.querySelector(".card__delete-button").addEventListener("click", () => card.remove());
-    card.querySelector(".card__like-button").addEventListener("click", e => {
-        e.currentTarget.classList.toggle("card__like-button_is-active")
-    });
+    cardImage.addEventListener("click", () => showContentImage(name, link));
+    card.querySelector(".card__delete-button").addEventListener("click", () => removeCard(card));
+    card.querySelector(".card__like-button").addEventListener("click", toggleLike);
 
     return card;
 };
 
+// Функция добавления карточки в DOM
+const addCardToPage = (cardData) => {
+    const card = createCard(cardData, {removeCard, toggleLike, showContentImage});
+    cards.prepend(card);
+};
+
 // Добавление карточки
-cardCreateSubmitButton.addEventListener("click", () => {
-    const newCard = createCard({name: cardName.value, link: cardUrl.value});
-    cards.append(newCard);
+cardCreateForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    addCardToPage({name: cardName.value, link: cardUrl.value});
     closePopup(cardCreatePopup);
     cardName.value = "";
     cardUrl.value = "";
-});
-
-// Открытие попапа редактирования профиля
-const profileEditButton = document.querySelector(".profile__edit-button");
-const profileEditPopup = document.querySelector(".popup_type_edit");
-profileEditButton.addEventListener("click", () => openPopup(profileEditPopup));
-
-const profileTitle = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
-const profileEditName = profileEditPopup.querySelector(".popup__input_type_name");
-const profileEditDescription = profileEditPopup.querySelector(".popup__input_type_description");
-const profileEditSubmitButton = profileEditPopup.querySelector(".popup__button");
-
-// Редактирование профиля
-profileEditSubmitButton.addEventListener("click", () => {
-    profileTitle.textContent = profileEditName.value;
-    profileDescription.textContent = profileEditDescription.value;
-    closePopup(profileEditPopup);
 });
 
 // Попап с изображением
@@ -70,12 +79,5 @@ const contentImagePopup = document.querySelector(".popup_type_image");
 const contentImage = contentImagePopup.querySelector(".popup__image");
 const contentCaption = contentImagePopup.querySelector(".popup__caption");
 
-// Функция показа изображения
-const showContentImage = card => {
-    contentImage.src = card.querySelector('.card__image').src;
-    contentCaption.textContent = card.querySelector('.card__title').textContent;
-    openPopup(contentImagePopup);
-};
-
 // Отображение карточек на странице
-initialCards.forEach(cardData => cards.append(createCard(cardData)));
+initialCards.forEach(addCardToPage);
